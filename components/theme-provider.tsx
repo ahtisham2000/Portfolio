f0a13e7c2ad8a13655d7eabb30687ba49948f441
ahtisham2000 <ahtishamulhaq2010@gmail.com>
2025-03-12 14:44:46 +0500

    default light mode

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,13 @@
-'use client'
+"use client";
 
-import * as React from 'react'
+import * as React from "react";
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
-} from 'next-themes'
+} from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider defaultTheme="light"  attribute="class" {...props}>
+    {children}
+    </NextThemesProvider>;
 }
