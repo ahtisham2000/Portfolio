bc5bcc037ef12eb5e8afd17a8e4d08cd569d2543
ahtisham2000 <ahtishamulhaq2010@gmail.com>
2025-03-13 11:46:20 +0500

    changes in layout img tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
       <head>
   <meta property="og:title" content="Ahtisham Ul Haq | Frontend Developer" />
   <meta property="og:description" content="Explore my portfolio showcasing React, Next.js, and Tailwind CSS projects." />
-  <meta property="og:image" content="https://ahtishamulhaq.vercel.app/preview-image-dark.png"/>
+  <meta name="image" property="og:image" content="https://ahtishamulhaq.vercel.app/preview-image-dark.png"/>
   <meta property="og:url" content="https://ahtishamulhaq.vercel.app/" />
   
 </head>
