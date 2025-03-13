4970663d870073333d4b7c0380c2845fad725c27
ahtisham2000 <ahtishamulhaq2010@gmail.com>
2025-03-13 11:42:27 +0500

    changes in layout meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   <meta property="og:title" content="Ahtisham Ul Haq | Frontend Developer" />
   <meta property="og:description" content="Explore my portfolio showcasing React, Next.js, and Tailwind CSS projects." />
   <meta property="og:image" content="https://ahtishamulhaq.vercel.app/preview-image-dark.png"/>
-    <meta property="og:url" content="https://ahtishamulhaq.vercel.app/" />
+  <meta property="og:url" content="https://ahtishamulhaq.vercel.app/" />
   
 </head>
       <body>{children}</body>
